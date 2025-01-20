13e2c1b62edb11998ce1ee8917fcdd57466af53f
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-01-20 22:32:46 +0530

    swap_two_numbers_using_three_variables

---
+++ SWAPNUMB.C
@@ -0,0 +1,21 @@
+//swap two numbers using third variable.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ clrscr();
+ printf("\n Enter first number");
+ scanf("\n%d",&a);
+ pr
+ intf("\nEnter second number");
+ scanf("\n%d",&b);
+ printf("\nEnter third number");
+ scanf("\n%d",&c);
+ a=b;
+ b=c;
+ c=a;
+ printf("\n Interchanged numbers using a third variable");
+ printf("\n a=%d \nb=%d",a,b);
+ getch();
+}
