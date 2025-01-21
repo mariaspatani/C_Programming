1d6ea076581c54e98896a04c99d44800cf99e44f
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-01-21 23:31:35 +0530

    sum of two numbers

---
+++ SUMTWONU.C
@@ -0,0 +1,18 @@
+/******************************
+* File        : addNumbers.C
+* Description : Add two numbers
+* Author      : Maria
+* Version     : 1.0
+* Date        : 21/01/2025
+********************************/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int number1=10,number2=20,sum;
+ clrscr();
+ sum=number1+number2;
+ printf("Sum of two Numbers=%d",sum);
+ getch();
+ return 0;
+}
