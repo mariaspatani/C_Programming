b3f8e08b04a21f91449b02f6bcc2c983adf6b80c
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-02-07 09:27:46 +0530

    lateral diagonal elements is zero

---
+++ 2DArray0.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int row,col,sum=0,i,j;
+	printf("\n Enter the elements in rows and columns");
+	scanf("%d%d",&row,&col);
+	int matrix[row][col];
+	printf("\n Enter the elements");
+	for(i=0;i<row;i++)
+	{
+		for(j=0;j<col;j++)
+		{
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+	for(i=0;i<row;i++)
+	{
+			for(j=0;j<col;j++)
+		       {
+			  if(i==j)
+			  {
+				matrix[i][j]=0;
+			  }
+			}
+			
+	}
+	printf("\n The matrix is:\n");
+	for(i=0;i<row;i++)
+	{
+			for(j=0;j<col;j++)
+			{
+			   printf(" %d\t",matrix[i][j]);
+			}
+			printf("\n");
+		
+	}
+	
+	return 0;
+}
+
+
+	
