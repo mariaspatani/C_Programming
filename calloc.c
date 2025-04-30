26f652b504e1febe24f446083a3edea5dd55faf8
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-04-30 14:34:15 +0530

    read array using calloc fn

---
+++ calloc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int *array;
+  int n;
+  printf("\n Enter the number of elements:");
+  scanf("%d",&n);
+  array =(int *) calloc(n,sizeof(int));
+  printf("\n Enter the elements:\n");
+  for(int i=0;i<n;i++){
+    scanf("%d",array+i);
+  }
+  for(int i=0;i<n;i++){
+    printf("%d\t",*(array+i));
+  }
+  return 0;
+}
