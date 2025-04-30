c205c90ce22c1dc54224e8bed819cb576ca7af24
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-04-30 15:44:42 +0530

    Add files via upload

---
+++ ptrArr2D.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int **matrix;
+  int row=3,col=5;
+  matrix = (int **)malloc(row * sizeof(int *));
+  for(int i=0;i<row;i++){
+    matrix[i]=(int *)malloc(col *sizeof(int));
+  }
+  printf("\n Enter the elements");
+  for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+         scanf("%d",(*(matrix+i)+j));
+      }
+  }
+  printf("\n The matrix is:");
+  for(int i=0;i<row;i++){
+      for(int j=0;j<col;j++){
+        printf("%d\t", *(*(matrix+i)+j));
+
+      }
+  }
+  printf("\n"); 
+  return 0;
+}
+
