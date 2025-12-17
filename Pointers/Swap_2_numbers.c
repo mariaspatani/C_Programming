cdd5cf54b554254d06d9785c2f72f60ddd935e81
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-18 00:37:27 +0530

    Create Swap_2_numbers.c

---
+++ Pointers/Swap_2_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a = 10, b = 20;
+    int *p1 = &a, *p2 = &b;
+    int temp;
+
+    temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
