cab6f0fc0f2c7189dabf38db072b9f5331df6ab0
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2026-01-05 00:05:54 +0530

    Create check argument count.c

---
+++ Command_Line_Arguments/check argument count.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3) {
+        printf("Usage: program.exe num1 num2");
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+
+    printf("Sum = %d", a + b);
+
+    return 0;
+}
